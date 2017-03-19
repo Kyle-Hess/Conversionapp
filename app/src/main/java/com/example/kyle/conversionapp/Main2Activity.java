15edf59f1ddfbea49217534ce2ed75d2e239e8d5
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2017-03-19 20:33:12 +1000

    -converting between the current 3 length types does work -can now change between units (length and speed) by pressing 'change unit' button and selecting a unit type

--- app/src/main/java/com/example/kyle/conversionapp/Main2Activity.java
+++ app/src/main/java/com/example/kyle/conversionapp/Main2Activity.java
@@ -1,13 +1,49 @@
 package com.example.kyle.conversionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button buttonLength;
+    private Button buttonSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        buttonLength = (Button)findViewById(R.id.buttonLength);
+        buttonSpeed = (Button)findViewById(R.id.buttonSpeed);
+
+
+
+        buttonLength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLength = new Intent(Main2Activity.this, MainActivity.class);
+                intentLength.putExtra("unit", "length");
+//                String length = "length";
+//                MainActivity mainActivity = new MainActivity();
+//                mainActivity.dataAdapter(length);
+                startActivity(intentLength);
+            }
+        });
+
+        buttonSpeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSpeed = new Intent(Main2Activity.this, MainActivity.class);
+                intentSpeed.putExtra("unit", "speed");
+//                String speed = "speed";
+//                MainActivity mainActivity = new MainActivity();
+//                mainActivity.dataAdapter(speed);
+                startActivity(intentSpeed);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kyle/conversionapp/MainActivity.java
+++ app/src/main/java/com/example/kyle/conversionapp/MainActivity.java
@@ -6,28 +6,63 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinnerFrom;
+    private TextView unitTitle;
+    public Spinner spinnerFrom;
     private Spinner spinnerTo;
     private EditText inputFrom;
     private EditText inputTo;
+    private ArrayAdapter<CharSequence>adapter1;
+    private ArrayAdapter<CharSequence>adapter2;
+
+    public String currentUnitType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        unitTitle = (TextView) findViewById(R.id.unit_title);
         spinnerFrom = (Spinner) findViewById(R.id.spinner_from);
         spinnerTo = (Spinner) findViewById(R.id.spinner_to);
+        //ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.length_array, android.R.layout.simple_spinner_item);
+        //ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.speed_array, android.R.layout.simple_spinner_item);
+
+        adapter1 = ArrayAdapter.createFromResource(this, R.array.length_array, android.R.layout.simple_spinner_item);
+        adapter2 = ArrayAdapter.createFromResource(this, R.array.speed_array, android.R.layout.simple_spinner_item);
+
+        spinnerFrom.setAdapter(adapter1);
+        spinnerTo.setAdapter(adapter1);
+
         inputFrom = (EditText) findViewById(R.id.input_from);
         inputTo = (EditText) findViewById(R.id.input_to);
 
+        Intent rIntent = getIntent();
+        currentUnitType = rIntent.getStringExtra("unit");
+        System.out.println(currentUnitType);
+        //// TODO: 19/03/2017 put the below switch statement into a method?
+        try {
+            switch (currentUnitType){
+                case "length":
+                    spinnerFrom.setAdapter(adapter1);
+                    spinnerTo.setAdapter(adapter1);
+                    break;
+                case "speed":
+                    spinnerFrom.setAdapter(adapter2);
+                    spinnerTo.setAdapter(adapter2);
+                    break;
+        }
+        }catch (Exception e){
+
+        }
 
         //Button to Unit selection screen
         Button unitButton = (Button) findViewById(R.id.button);
@@ -35,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                //intent.putExtra();
                 startActivity(intent);
             }
         });
@@ -49,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence string, int start, int before, int count) {
                 try {
+                    //// TODO: 19/03/2017 Mabey try to simplify into a single method.
                     String spinnerUnitFrom = (String) spinnerFrom.getSelectedItem();
                     String spinnerUnitTo = (String) spinnerTo.getSelectedItem();
                     double value = Integer.parseInt(string.toString());
@@ -70,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
         inputTo.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence string, int start, int count, int after) {
-
             }
 
             @Override
@@ -84,7 +120,6 @@ public class MainActivity extends AppCompatActivity {
                     inputFrom.setText(String.valueOf(result));
                 } catch (Exception e) {
                 }
-
             }
 
             @Override
@@ -96,16 +131,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private double convert(double value, String spinnerUnitFrom, String spinnerUnitTo) {
-
+//// TODO: 19/03/2017 find better naming for spinnerUnit...
         double num1 = value;
         double num2 = 0.0;
 
-        String from = spinnerUnitFrom;
-        String to = spinnerUnitTo;
-
-        switch (from) {
+        switch (spinnerUnitFrom) {
             case "Metre": {
-                switch (to) {
+                switch (spinnerUnitTo) {
                     case "Metre":
                         num2 = num1;
                         break;
@@ -119,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case "Centimetre": {
-                switch (to) {
+                switch (spinnerUnitTo) {
                     case "Centimetre":
                         num2 = num1;
                         break;
@@ -133,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case "Inches": {
-                switch (to) {
+                switch (spinnerUnitTo) {
                     case "Inches":
                         num2 = num1;
                         break;
@@ -147,6 +179,17 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return num2;
+    }
+
+    public void dataAdapter(String text){
+//// TODO: 19/03/2017 try to use intent
+        if (text.equals("length")){
+            spinnerFrom.setAdapter(adapter1);
+            spinnerTo.setAdapter(adapter1);
+        }else if (text.equals("speed")){
+            spinnerFrom.setAdapter(adapter2);
+            spinnerTo.setAdapter(adapter2);
+        }
     }
 }
 
